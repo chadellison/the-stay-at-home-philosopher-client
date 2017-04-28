ad50226c3e5ac0888922248f37b1ca6c8eaeb47a
Chad Ellison <chad.ellison0123@gmail.com>
2017-04-27 21:28:37 -0600

    add pagination arrows

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Post from './Post.js'
 import "../styles/posts.css"
+import PageArrows from './PageArrows.js'
 
 export default class Posts extends Component {
   render() {
     self = this
     return (
       <div className={"posts" + this.props.opacity}>
+        <PageArrows />
         { this.props.posts.map(function(post) {
           return (
             <Post
